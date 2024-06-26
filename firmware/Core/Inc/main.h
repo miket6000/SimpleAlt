fff8747b851f6d4df19184cc31f628a30dc6c4ae
Michael Turner <miket6000@gmail.com>
2024-06-26 20:58:15 +1200

    Adding autogenerated firmware from CubeMX with hardware peripherals configured.

---
+++ firmware/Core/Inc/main.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define nSENSE_EN_Pin GPIO_PIN_1
+#define nSENSE_EN_GPIO_Port GPIOA
+#define CHARGE_EN_Pin GPIO_PIN_2
+#define CHARGE_EN_GPIO_Port GPIOA
+#define nCHARGE_Pin GPIO_PIN_3
+#define nCHARGE_GPIO_Port GPIOA
+#define SHUTDOWN_Pin GPIO_PIN_4
+#define SHUTDOWN_GPIO_Port GPIOA
+#define BATT_SENSE_Pin GPIO_PIN_6
+#define BATT_SENSE_GPIO_Port GPIOA
+#define BMP_CS_Pin GPIO_PIN_15
+#define BMP_CS_GPIO_Port GPIOA
+#define FLASH_CS_Pin GPIO_PIN_7
+#define FLASH_CS_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
