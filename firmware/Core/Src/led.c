823e42f0b5f51755ed1ebcbdc26502eac66812aa
Michael Turner <miket6000@gmail.com>
2024-07-16 21:09:58 +1200

    changed 'queue' to 'sequence' in variable names and added a short pause

--- firmware/Core/Src/led.c
+++ firmware/Core/Src/led.c
@@ -1,9 +1,10 @@
 #include "led.h"
 #include "gpio.h"
-#define BLINK_OFF_TIME 20
-#define MAX_QUEUE_LEN 10
-#define PAUSE 10
-#define END_OF_QUEUE 0xFF
+#define BLINK_OFF_TIME 17
+#define MAX_SEQUENCE_LEN 10
+#define SHORT_PAUSE 10
+#define PAUSE 11
+#define END_OF_SEQUENCE 0xFF
 #define MAX_INDEX (32 / 2 - 1)
 
 
@@ -21,11 +22,17 @@ void Led(led_state_t state) {
   }
 }
 
-uint32_t blink_queue[] = {3, 5, 7, 0, PAUSE, END_OF_QUEUE};
-uint8_t queue_index = 0;
+uint32_t blink_sequence[] = {3, 5, 7, 0, PAUSE, END_OF_SEQUENCE};
+uint8_t sequence_index = 0;
 uint8_t blink_index = 0;
 uint8_t blink_off_counter = BLINK_OFF_TIME;
 
+/* Seqences are read right to left, two bits at a time. The first bit of each pair
+ * indicates that this is part of the current sequence. The second bit indicates 
+ * whether the LED is on (1) or off (0).
+ * LEDs are on for 1 cycle, then off for BLINK_OFF_TIME cycles. 
+ * Cycle length is determined by time between calls to Led_Blink().
+ */
 uint32_t codes[] = {  
   0x0AAFFFFF, //0
   0x000000AB, //1
@@ -37,6 +44,7 @@ uint32_t codes[] = {
   0x000ABFFF, //7
   0x00AAFFFF, //8
   0x00ABFFFF, //9
+  0x0000AAAA, //SHORT_PAUSE
   0xAAAAAAAA  //PAUSE
 };
 
@@ -45,7 +53,7 @@ void Led_Blink() {
   uint8_t blink;
 
   if (blink_off_counter == 0) {
-    code = codes[blink_queue[queue_index]];
+    code = codes[blink_sequence[sequence_index]];
     blink = (code >> (2 * blink_index)) & 0x00000003;
     
     if (blink & 0x01) {
@@ -56,9 +64,9 @@ void Led_Blink() {
       blink_index++;
     } else {
       blink_index = 0;
-      queue_index++;
-      if (queue_index > MAX_QUEUE_LEN || blink_queue[queue_index] == END_OF_QUEUE) {
-        queue_index = 0;
+      sequence_index++;
+      if (sequence_index > MAX_SEQUENCE_LEN || blink_sequence[sequence_index] == END_OF_SEQUENCE) {
+        sequence_index = 0;
       }
     }
 
