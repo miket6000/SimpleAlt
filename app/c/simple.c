b5ee9dfd7dbbccd82444b80d0dee0d64a96f067c
Michael Turner <miket6000@gmail.com>
2024-08-25 22:02:27 +1200

    added flash dump capability to c app.

--- app/c/simple.c
+++ app/c/simple.c
@@ -23,7 +23,9 @@ int main(int argc, char **argv) {
   
   char *port_name = argv[1];
 
-  altimeter_connect(port_name);
+  if (altimeter_connect(port_name) >= 0) {
+    printf("Succsefully to %s\n", port_name);
+  }
 
   printf("The following recordings were found:\n\n");
 
@@ -36,15 +38,10 @@ int main(int argc, char **argv) {
   /* get and validate user decision on which recording to download */
   uint32_t recording_id = 0;
   scanf("%d", &recording_id);
-  if (recording_id < 1 || recording_id > altimeter_get_recording_count()) {
+  if (recording_id > altimeter_get_recording_count()) {
     printf("Sorry, the input was not recognised.\n");
     return (-1);
   }
-
-  /* prepare and download the recording */
-  uint32_t recording_length = altimeter_get_recording_length(recording_id - 1);
-  int32_t *altitudes = (int32_t *)malloc(recording_length * sizeof(int32_t));
-  altimeter_get_recording(altitudes, recording_id - 1, recording_length);
 
   /* determine a filename that's descriptive, but not already in use */
   char filename[64];
@@ -54,19 +51,33 @@ int main(int argc, char **argv) {
   while (access(filename, F_OK) == 0) {
     sprintf(filename, "SimpleAlt_flight_%i_%i.csv", recording_id, file_append++);
   }
-
   /* save the file */
-  FILE *fpt = fopen(filename, "w+");
-  fprintf(fpt, "Time, Altitude\n");
-  for (int i = 0; i < recording_length; i++) {
-    fprintf(fpt, "%0.2f, %0.2f\n", i * 0.02, (altitudes[i]-altitudes[0])/100.0);
-  }
-  fclose(fpt);
 
+  if (recording_id == 0) {
+    FILE *fpt = fopen(filename, "w+b");
+    uint8_t *data = (uint8_t *)malloc(0x00ffffff);
+    altimeter_get_data(data, 0, 0x00ffffff);
+    fwrite(data, sizeof(uint8_t), 0x00ffffff, fpt);
+    fclose(fpt);
+    free(data);
+  } else {
+    FILE *fpt = fopen(filename, "w+");
+    /* prepare and download the recording */
+    uint32_t recording_length = altimeter_get_recording_length(recording_id - 1);
+    int32_t *altitudes = (int32_t *)malloc(recording_length * sizeof(int32_t));
+    altimeter_get_recording(altitudes, recording_id - 1, recording_length);
+    fprintf(fpt, "Time, Altitude\n");
+    for (int i = 0; i < recording_length; i++) {
+      fprintf(fpt, "%0.2f, %0.2f\n", i * 0.02, (altitudes[i]-altitudes[0])/100.0);
+    }
+    fclose(fpt);
+    free(altitudes);
+      
+  }
+ 
   printf("File saved as %s\n", filename);
 
   /* tidy up and return successfully */
-  free(altitudes);
   return 0;
 }
 
