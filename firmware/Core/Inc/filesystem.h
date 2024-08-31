c9047e174bd667639d50fd8239f5d5b5e0d64eae
Nick Lott <nick.lott@gmail.com>
2024-09-01 11:31:06 +1200

    Some tidy up in filesystem module.
    * Make file global variables static scope and add g_ prefix to indicate global variables
    * Make parameters to fs_save() const where possible.
    * Add doxygen comments to fs_save()

--- firmware/Core/Inc/filesystem.h
+++ firmware/Core/Inc/filesystem.h
@@ -15,7 +15,14 @@ void fs_init(SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port, uint16_t cs_pin);
 void fs_stop();
 void fs_open();
 void fs_close();
-void fs_save(char label, void *data, uint16_t len);
+
+/**!
+ *  @brief Save a data record
+ *  @param label Single character label for the data item.
+ *  @param data  Pointer to the data to be saved
+ *  @param len   size of the data in bytes
+*/
+void fs_save(char const label, void * const  data, uint16_t const len);
 
 void fs_raw_read(uint32_t address, uint8_t *buffer, uint16_t len);
 void fs_raw_write(uint32_t address, uint8_t *buffer, uint16_t len);
